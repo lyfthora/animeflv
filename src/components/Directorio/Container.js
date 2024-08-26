5eec2d795e3ec8dc2d95c0daa03c6d64b63bb639
Briarivero <marcosbrayan03@gmail.com>
2024-08-26 12:42:12 +0200

    fix directorio.

--- src/components/Directorio/Container.js
+++ src/components/Directorio/Container.js
@@ -1,52 +1,33 @@
-import React, { Fragment, useRef, useEffect, useState } from "react";
-import cover from "../../img/cover.jpg";
-
-function useOutsideClick(ref, callback, when) {
-  const savedCallback = useRef(callback);
-
-  useEffect(() => {
-    savedCallback.current = callback;
-  });
-
-  function handler(e) {
-    if (ref.current && !ref.current.contains(e.target)) {
-      savedCallback.current();
-    }
-  }
-
-  useEffect(() => {
-    if (when) {
-      document.addEventListener("click", handler);
-      return () => document.removeEventListener("click", handler);
-    }
-  }, [when]);
-}
+import React, { Fragment, useState } from "react";
+import Cover from "../../img/cover.jpg";
 
 const Container = () => {
+  // Estado para manejar la visibilidad del menú de géneros
+  const [isGenresVisible, setIsGenresVisible] = useState(false);
+
+  // Función para alternar la visibilidad del menú
+  const toggleGenresVisibility = () => {
+    setIsGenresVisible(!isGenresVisible);
+  };
+
   const newObj = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   const gnro = [
-    "Acción",
+    "Acción",
     "Aventura",
     "Comedia",
-    "Recuentos de la vida",
-    "Escolares",
-    "Ecchi",
-    "Harem",
+    "Drama",
+    "Fantasía",
+    "Histórico",
+    "Misterio",
+    "Romance",
+    "Suspense",
+    "Terror",
   ];
 
-  const [gen, setGen] = useState(false);
-
-  const dropdownGen = useRef(null);
-
-  function showDropdownGen() {
-    setGen(gnro);
-  }
-
-  function hideDropdownGen() {
-    setGen(false);
-  }
-
-  useOutsideClick(dropdownGen, hideDropdownGen, gen);
+  // Manejar el evento submit del formulario para evitar el refresh
+  const handleFormSubmit = (event) => {
+    event.preventDefault(); // Evita que la página se refresque
+  };
 
   return (
     <Fragment>
@@ -57,48 +38,48 @@ const Container = () => {
               <h1>Lista completa de Animes</h1>
             </div>
             <main>
-              <form>
+              <form onSubmit={handleFormSubmit}>
                 <div className="filters">
                   <span className="multiselect-filter">
-                    <div
+                    <button
+                      type="button"
                       className="btn-multiselect"
-                      onClick={gen ? hideDropdownGen : showDropdownGen}
+                      onClick={toggleGenresVisibility}
                     >
                       <span className="multiselect-selected-text">
-                        <b>Genero: </b>
-                        {gen ? gen[0] : "Todos"}
+                        <b>Género: </b>
+                        Todos
                       </span>
                       <b className="caret"></b>
-                    </div>
-                    {gen && (
-                      <div className="multiselect-options">
-                        {gnro.map((g, i) => (
-                          <span
-                            key={i}
-                            className="multiselect-option"
-                            onClick={() => setGen([g])}
-                          >
-                            {g}
-                          </span>
+                    </button>
+                    {isGenresVisible && (
+                      <div className="multiselect-container zoomIn">
+                        {gnro.map((obj) => (
+                          <div className="select-checkbox" key={obj}>
+                            <input type="checkbox" id={obj} />
+                            <label
+                              htmlFor={obj}
+                              className="filter-select-checkbox"
+                            >
+                              {obj}
+                            </label>
+                          </div>
                         ))}
                       </div>
                     )}
                   </span>
-                  <button type="submit" className="btn-filter-submit">
-                    <i className="fas fa-filter"></i> FILTRAR
-                  </button>
                 </div>
               </form>
               <ul className="last-animes">
                 {newObj.map((obj, idx) => (
-                  <li key={idx} style={{ width: "16.66667%" }}>
+                  <li key={idx}>
                     <article className="anime">
                       <span className="estreno">
                         <span>ESTRENO</span>
                       </span>
                       <div className="image fa-play-circle-o">
                         <figure>
-                          <img src={cover} alt="cover" />
+                          <img src={Cover} alt="cover" />
                         </figure>
                         <span className="type-anime tv">ANIME</span>
                       </div>
@@ -107,19 +88,6 @@ const Container = () => {
                   </li>
                 ))}
               </ul>
-              <div style={{ textAlign: "center" }}>
-                <div className="pagination">
-                  <a>
-                    <i class="fas fa-angle-double-left"></i>
-                  </a>
-                  <a>1</a>
-                  <a>2</a>
-                  <a>3</a>
-                  <a>
-                    <i class="fas fa-angle-double-right"></i>
-                  </a>
-                </div>
-              </div>
             </main>
           </div>
         </div>
