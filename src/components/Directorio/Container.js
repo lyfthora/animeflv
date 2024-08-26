401163ee039160176bfdefcc7d5e333951c657fd
Briarivero <marcosbrayan03@gmail.com>
2024-08-26 03:19:52 +0200

    fix.

--- src/components/Directorio/Container.js
+++ src/components/Directorio/Container.js
@@ -1,19 +1,53 @@
-import React, { Fragment } from "react";
-import Cover from "../../img/cover.jpg";
+import React, { Fragment, useRef, useEffect, useState } from "react";
+import cover from "../../img/cover.jpg";
+
+function useOutsideClick(ref, callback, when) {
+  const savedCallback = useRef(callback);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  function handler(e) {
+    if (ref.current && !ref.current.contains(e.target)) {
+      savedCallback.current();
+    }
+  }
+
+  useEffect(() => {
+    if (when) {
+      document.addEventListener("click", handler);
+      return () => document.removeEventListener("click", handler);
+    }
+  }, [when]);
+}
+
 const Container = () => {
   const newObj = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   const gnro = [
-    "Acción",
+    "Acción",
     "Aventura",
     "Comedia",
-    "Drama",
-    "Fantasía",
-    "Histórico",
-    "Misterio",
-    "Romance",
-    "Suspense",
-    "Terror",
+    "Recuentos de la vida",
+    "Escolares",
+    "Ecchi",
+    "Harem",
   ];
+
+  const [gen, setGen] = useState(false);
+
+  const dropdownGen = useRef(null);
+
+  function showDropdownGen() {
+    setGen(gnro);
+  }
+
+  function hideDropdownGen() {
+    setGen(false);
+  }
+
+  useOutsideClick(dropdownGen, hideDropdownGen, gen);
+
   return (
     <Fragment>
       <section className="container-principal">
@@ -26,39 +60,45 @@ const Container = () => {
               <form>
                 <div className="filters">
                   <span className="multiselect-filter">
-                    <button className="btn-multiselect">
+                    <div
+                      className="btn-multiselect"
+                      onClick={gen ? hideDropdownGen : showDropdownGen}
+                    >
                       <span className="multiselect-selected-text">
                         <b>Genero: </b>
-                        Todos
+                        {gen ? gen[0] : "Todos"}
                       </span>
                       <b className="caret"></b>
-                    </button>
-                    <div className="multiselect-container zoomIn">
-                      {gnro.map((obj) => (
-                        <div className="select-checkbox">
-                          <input type="checkbox" id={obj} />
-                          <label
-                            htmlFor={obj}
-                            className="filter-select-checkbox"
-                          >
-                            {obj}
-                          </label>
-                        </div>
-                      ))}
                     </div>
+                    {gen && (
+                      <div className="multiselect-options">
+                        {gnro.map((g, i) => (
+                          <span
+                            key={i}
+                            className="multiselect-option"
+                            onClick={() => setGen([g])}
+                          >
+                            {g}
+                          </span>
+                        ))}
+                      </div>
+                    )}
                   </span>
+                  <button type="submit" className="btn-filter-submit">
+                    <i className="fas fa-filter"></i> FILTRAR
+                  </button>
                 </div>
               </form>
               <ul className="last-animes">
                 {newObj.map((obj, idx) => (
-                  <li key={idx}>
+                  <li key={idx} style={{ width: "16.66667%" }}>
                     <article className="anime">
                       <span className="estreno">
                         <span>ESTRENO</span>
                       </span>
                       <div className="image fa-play-circle-o">
                         <figure>
-                          <img src={Cover} alt="cover" />
+                          <img src={cover} alt="cover" />
                         </figure>
                         <span className="type-anime tv">ANIME</span>
                       </div>
@@ -67,6 +107,19 @@ const Container = () => {
                   </li>
                 ))}
               </ul>
+              <div style={{ textAlign: "center" }}>
+                <div className="pagination">
+                  <a>
+                    <i class="fas fa-angle-double-left"></i>
+                  </a>
+                  <a>1</a>
+                  <a>2</a>
+                  <a>3</a>
+                  <a>
+                    <i class="fas fa-angle-double-right"></i>
+                  </a>
+                </div>
+              </div>
             </main>
           </div>
         </div>
